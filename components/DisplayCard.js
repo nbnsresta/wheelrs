5010897d7ff7d738d11c66889c5de90fba8f3665
Nabin Shrestha <nabin.shrestha@renegadeinsurance.com>
2022-07-08 01:06:20 +0545

    feat: implement verical orientation in product compare table

--- components/DisplayCard.js
+++ components/DisplayCard.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 const DisplayCard = ({ imageUrl, productName, href }) => {
   return (
     <Link href={href} passHref>
-      <a className="flex-col p-4 rounded-md shadow-md hover:shadow-lg hover:-translate-y-1 transition-transform ease-linear duration-400">
+      <a className="flex flex-col rounded-md shadow-md hover:shadow-lg hover:-translate-y-1 transition-transform ease-linear duration-400 overflow-hidden">
         <Image
           src={imageUrl}
           alt={productName}
--- components/ProductTable.js
+++ components/ProductTable.js
@@ -1,67 +1,97 @@
-import Link from "next/link";
 import cx from "classnames";
+import get from "lodash-es/get";
+import Image from "next/image";
 
-const Th = (props) => {
-  return <th className="p-4 whitespace-nowrap" {...props} />;
+const TitleCell = (props) => {
+  return (
+    <td className="p-4 whitespace-nowrap bg-gray-900 text-white" {...props} />
+  );
 };
 
-const Td = ({ className, ...props }) => {
+const DataCell = ({ className, ...props }) => {
   return (
     <td
-      className={cx("p-4 whitespace-nowrap text-center rounded-md", className)}
+      className={cx("p-4 whitespace-nowrap rounded-md", className)}
       {...props}
     />
   );
 };
 
-const ProductTable = ({ products }) => {
+const rows = [
+  {
+    field: "name",
+    label: "Name",
+    formatter: (value) => <span className="font-bold text-lg">{value}</span>,
+  },
+  { field: "brand.name", label: "Brand" },
+  { field: "bodyType.name", label: "Body Type" },
+  { field: "EngineType.name", label: "Engine Type" },
+  {
+    field: "displacement",
+    label: "Displacement",
+    formatter: (value) => `${value} cc`,
+  },
+  { field: "mileage", label: "Mileage", formatter: (value) => `${value} kmpl` },
+  {
+    field: "fuelCapacity",
+    label: "Fuel Capacity",
+    formatter: (value) => `${value} litres`,
+  },
+  { field: "cylinders", label: "No. of Cylinders" },
+  { field: "maxPower", label: "Max Power" },
+  { field: "maxTorque", label: "Max Torque" },
+  { field: "frontBrake", label: "Front Brake" },
+  { field: "rearBrake", label: "Rear Brake" },
+  { field: "instrumentCluster", label: "Instrument Cluster" },
+  {
+    field: "seatHeight",
+    label: "Seat Height",
+    formatter: (value) => `${value} mm`,
+  },
+  {
+    field: "groundClearance",
+    label: "Ground Clearance",
+    formatter: (value) => `${value} mm`,
+  },
+  { field: "suspensionType", label: "Suspension Type" },
+  {
+    field: "kerbWeight",
+    label: "Kerb Weight",
+    formatter: (value) => `${value} kg`,
+  },
+];
+
+const ProductTable = ({ products, getOptions }) => {
   return (
     <table className="table-fixed border-separate">
-      <thead className="bg-black text-white p-2">
-        <tr>
-          <Th>Name</Th>
-          <Th>Brand</Th>
-          <Th>Body Type</Th>
-          <Th>Engine Type</Th>
-          <Th>Displacement</Th>
-          <Th>Mileage</Th>
-          <Th>Fuel Capacity</Th>
-          <Th>No. of Cylinders</Th>
-          <Th>Max Power</Th>
-          <Th>Max Torque</Th>
-          <Th>Front Brake</Th>
-          <Th>Rear Brake</Th>
-          <Th>Instrument Cluster</Th>
-          <Th>Seat Height</Th>
-          <Th>Ground Clearance</Th>
-          <Th>Suspension Type</Th>
-          <Th>Kerb Weight</Th>
-        </tr>
-      </thead>
       <tbody>
-        {products.map((product) => (
-          <tr key={product.id}>
-            <Td className="hover:bg-sky-500 focus:ring-sky-500 hover:text-white">
-              <Link href={`/product/${product.id}`} passHref>
-                <a className="-m-4 p-4">{product.name}</a>
-              </Link>
-            </Td>
-            <Td>{product.brand.name}</Td>
-            <Td>{product.bodyType.name}</Td>
-            <Td>{product.EngineType.name}</Td>
-            <Td>{`${product.displacement}cc`}</Td>
-            <Td>{product.mileage}</Td>
-            <Td>{product.fuelCapacity}</Td>
-            <Td>{product.cylinders}</Td>
-            <Td>{product.maxPower}</Td>
-            <Td>{product.maxTorque}</Td>
-            <Td>{product.frontBrake}</Td>
-            <Td>{product.rearBrake}</Td>
-            <Td>{product.instrumentCluster}</Td>
-            <Td>{`${product.seatHeight}mm`}</Td>
-            <Td>{`${product.groundClearance}mm`}</Td>
-            <Td>{product.suspensionType}</Td>
-            <Td>{`${product.kerbWeight}kg`}</Td>
+        <tr></tr>
+        <tr>
+          <td />
+          {products.map((product) => (
+            <td key={product.id}>
+              <Image
+                src={product.imageUrl}
+                alt={product.name}
+                width="300px"
+                height="200px"
+                quality={100}
+                objectFit="cover"
+              />
+            </td>
+          ))}
+        </tr>
+        {rows.map((row) => (
+          <tr key={row.field}>
+            <TitleCell>{row.label}</TitleCell>
+            {products.map((product) => {
+              const value = get(product, row.field);
+              return (
+                <DataCell key={product.id}>
+                  {row.formatter ? row.formatter(value) : value}
+                </DataCell>
+              );
+            })}
           </tr>
         ))}
       </tbody>
